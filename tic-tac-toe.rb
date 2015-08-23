d3f7bd89395bed2cd9697a8826ef7ba0c0f57873
Panashe M. Fundira <fundirap@gmail.com>
2015-08-23 12:43:12 -0400

    Return players to context of game

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,82 +1,80 @@
 # A playable tic tac toe game
-module TicTacToe
-  class Game
-    def initialize
-      @board = [[1, 2, 3],
-                [4, 5, 6],
-                [7, 8, 9]]
-        puts "Welcome to tic-tac 3000! Please select a game mode below."
-      begin
-        puts "1) Computer vs Computer"
-        puts "2) Human vs Computer"
-        puts "3) Human vs Human"
-        game_choice = gets.chomp.to_i
-        raise ArgumentError, "Please enter a valid number between 1 and 3" unless
-          (1..3).include?(game_choice)
-      rescue ArgumentError=>e
-        puts e
-        retry
-      end
-      case game_choice
-      when 1
-        @p1 = ComputerPlayer.new(:X)
-        @p2 = ComputerPlayer.new(:O)
-      when 2
-        @p1 = HumanPlayer.new(:X)
-        @p2 = ComputerPlayer.new(:O)
-      when 3
-        @p1 = HumanPlayer.new(:X)
-        @p2 = HumanPlayer.new(:O)
-      end
-      play
+class TicTacToe
+  def initialize
+    @board = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]]
+    puts "Welcome to tic-tac 3000! Please select a game mode below."
+    begin
+      puts "1) Computer vs Computer"
+      puts "2) Human vs Computer"
+      puts "3) Human vs Human"
+      game_choice = gets.chomp.to_i
+      raise ArgumentError, "Please enter a valid number between 1 and 3" unless
+      (1..3).include?(game_choice)
+    rescue ArgumentError=>e
+      puts e
+      retry
     end
+    case game_choice
+    when 1
+      @p1 = ComputerPlayer.new(:X)
+      @p2 = ComputerPlayer.new(:O)
+    when 2
+      @p1 = HumanPlayer.new(:X)
+      @p2 = ComputerPlayer.new(:O)
+    when 3
+      @p1 = HumanPlayer.new(:X)
+      @p2 = HumanPlayer.new(:O)
+    end
+    play
+  end
 
-    # checks whether or not the game is complete
-    def complete?
-      @board.each do |row|
-        row.each do |cell|
-          return false unless (:X, :O).include? cell
-        end
-      end
-      true
-    end
-
-    # draws the current state of the game board
-    def draw
-      rows_for_display = []
-      @board.each do |row|
-        rows_for_display << row.join(" | ")
-      end
-      rows_for_display.join("\n--+---+--\n")
-    end
-    
-    # validates the availability of a given position
-    def valid_move(pos)
-      case pos
-      when 1..3
-        return @board[0][pos-1].integer?
-      when 4..6
-        return @board[1][pos-4].integer?
-      when 7..9
-        return @board[2][pos-4].integer? 
-      else
-        return false
+  # checks whether or not the game is complete
+  def complete?
+    @board.each do |row|
+      row.each do |cell|
+        return false unless (:X, :O).include? cell
       end
     end
+    true
+  end
 
-    # method that controls the game loop
-    def play
-      loop do
+  # draws the current state of the game board
+  def draw
+    rows_for_display = []
+    @board.each do |row|
+      rows_for_display << row.join(" | ")
+    end
+    rows_for_display.join("\n--+---+--\n")
+  end
 
-      end
+  # validates the availability of a given position
+  def valid_move(pos)
+    case pos
+    when 1..3
+      return @board[0][pos-1].integer?
+    when 4..6
+      return @board[1][pos-4].integer?
+    when 7..9
+      return @board[2][pos-4].integer? 
+    else
+      return false
     end
   end
 
+  # method that controls the game loop
+  def play
+    loop do
+
+    end
+  end
+
+
   class Player
     attr_reader :symbol
-    def initialize(game, sym)
+    def initialize(game)
       @symbol = sym
-      @game = game
     end
   end
 
