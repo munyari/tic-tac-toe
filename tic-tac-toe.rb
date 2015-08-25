76bfbb2c9cd0b9e73a0b2263c7472284d9cf8421
Panashe M. Fundira <fundirap@gmail.com>
2015-08-25 16:37:07 -0400

    Add mark method, move player classes back outside Game class
    Turns out ruby doesn't have nested classes, so moving player classes
    inside the Game class did not allow any access to the game board and
    therefore had no benefit

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,86 +1,102 @@
 # A playable tic tac toe game
-class TicTacToe
-  def initialize
-    @board = [[1, 2, 3],
-              [4, 5, 6],
-              [7, 8, 9]]
-    puts "Welcome to tic-tac 3000! Please select a game mode below."
-    begin
-      puts "1) Computer vs Computer"
-      puts "2) Human vs Computer"
-      puts "3) Human vs Human"
-      game_choice = gets.chomp.to_i
-      raise ArgumentError, "Please enter a valid number between 1 and 3" unless
-      (1..3).include?(game_choice)
-    rescue ArgumentError=>e
-      puts e
-      retry
+module TicTacToe
+  class Game
+    def initialize
+      @board = [[1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]]
+      puts "Welcome to tic-tac 3000! Please select a game mode below."
+      begin
+        puts "1) Computer vs Computer"
+        puts "2) Human vs Computer"
+        puts "3) Human vs Human"
+        game_choice = gets.chomp.to_i
+        raise ArgumentError, "Please enter a valid number between 1 and 3" unless
+        (1..3).include?(game_choice)
+      rescue ArgumentError=>e
+        puts e
+        retry
+      end
+      case game_choice
+      when 1
+        @p1 = ComputerPlayer.new(self, :X)
+        @p2 = ComputerPlayer.new(self, :O)
+      when 2
+        @p1 = HumanPlayer.new(self, :X)
+        @p2 = ComputerPlayer.new(self, :O)
+      when 3
+        @p1 = HumanPlayer.new(self, :X)
+        @p2 = HumanPlayer.new(self, :O)
+      end
+      play
     end
-    case game_choice
-    when 1
-      @p1 = ComputerPlayer.new(self, :X)
-      @p2 = ComputerPlayer.new(self, :O)
-    when 2
-      @p1 = HumanPlayer.new(self, :X)
-      @p2 = ComputerPlayer.new(self, :O)
-    when 3
-      @p1 = HumanPlayer.new(self, :X)
-      @p2 = HumanPlayer.new(self, :O)
-    end
-    play
-  end
 
-  # checks whether or not the game is complete
-  def complete?
-    @board.each do |row|
-      row.each do |cell|
-        return false unless (:X, :O).include? cell
+    # checks whether or not the game is complete
+    def complete?
+      @board.each do |row|
+        row.each do |cell|
+          return false unless (:X, :O).include? cell
+        end
+      end
+      true
+    end
+
+    # check if we have a winner
+    def check_win
+      # make this DRY, could i use a proc?
+      @board.each           { |row| return true if row.all? { |sym| sym == "X" } }
+      @board.each           { |row| return true if row.all? { |sym| sym == "O" } }
+      @board.transpose.each { |col| return true if row.all? { |sym| sym == "X" } }
+      @board.transpose.each { |col| return true if row.all? { |sym| sym == "O" } }
+      @board[0][0] == @board[1][1] && @board[1][1] == @board[2][2] ||
+        @board[2][0] == @board[1][1] && @board[1][1] == @board[0][2]
+    end
+    # draws the current state of the game board
+    def draw
+      rows_for_display = []
+      @board.each do |row|
+        rows_for_display << row.join(" | ")
+      end
+      rows_for_display.join("\n--+---+--\n")
+    end
+
+    # validates the availability of a given position
+    def valid_move(pos)
+      case pos
+      when 1..3
+        return @board[0][pos-1].integer?
+      when 4..6
+        return @board[1][pos-4].integer?
+      when 7..9
+        return @board[2][pos-7].integer? 
+      else
+        return false
       end
     end
-    true
-  end
 
-  # check if we have a winner
-  def check_win
-    # make this DRY, could i use a proc?
-    @board.each           { |row| return true if row.all? { |sym| sym == "X" } }
-    @board.each           { |row| return true if row.all? { |sym| sym == "O" } }
-    @board.transpose.each { |col| return true if row.all? { |sym| sym == "X" } }
-    @board.transpose.each { |col| return true if row.all? { |sym| sym == "O" } }
-    @board[0][0] == @board[1][1] && @board[1][1] == @board[2][2] ||
-      @board[2][0] == @board[1][1] && @board[1][1] == @board[0][2]
-  end
-  # draws the current state of the game board
-  def draw
-    rows_for_display = []
-    @board.each do |row|
-      rows_for_display << row.join(" | ")
-    end
-    rows_for_display.join("\n--+---+--\n")
-  end
+    # mark the game board with the appropriate symbol
+    # TODO: make this and previous method DRY
+    def mark(pos, symbol)
+      case pos
+      when 1..3
+        @board[0][pos-1] = symbol
+      when 4..6
+        @board[1][pos-4] = symbol
+      when 7..9
+        @board[2][pos-7] = symbol
+      end
 
-  # validates the availability of a given position
-  def valid_move(pos)
-    case pos
-    when 1..3
-      return @board[0][pos-1].integer?
-    when 4..6
-      return @board[1][pos-4].integer?
-    when 7..9
-      return @board[2][pos-4].integer? 
-    else
-      return false
-    end
-  end
+    # method that controls the game loop
+    def play
+      current_player = @p1
+      loop do
+        current_player.play
+        break if complete?
+        current_player = current_player == @p1 : @p2
+      end
 
-  # method that controls the game loop
-  def play
-    current_player = @p1
-    loop do
-      current_player.play
-      break if complete?
-      current_player = current_player == @p1 : @p2
     end
+
 
   end
 
@@ -106,7 +122,7 @@ class TicTacToe
         puts "Sorry, that is not a valid move. Please try again."
         pos = gets.chomp
       end
-      pos
+      @game.mark(pos, @symbol.to_s)
     end
   end
 
@@ -119,9 +135,10 @@ class TicTacToe
       begin
         pos = Random.rand(10)
       end until @game.valid_move?(pos)
-      pos
+      @game.mark(pos, @symbol.to_s)
     end
   end
 end
 
-
+# TODO: Valid move setting
+Game.new
